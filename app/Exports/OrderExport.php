1f05aba028cdb74f9b5dda6e0dc86ed9f52e7ad3
jaydeep-adsum <jaydeep.adsum@gmail.com>
2022-07-18 12:37:46 +0530

    order export

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -5,14 +5,52 @@ namespace App\Exports;
 use App\Models\Order;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class OrderExport implements FromCollection
+class OrderExport implements FromCollection, WithHeadings
 {
     /**
     * @return Collection
     */
     public function collection(): Collection
     {
-        return Order::all();
+        $order= Order::all();
+        $data = [];
+        foreach ($order as $item){
+            $data[] = [
+                'id'=>$item->id,
+                'location'=>$item->location,
+                'latitude'=>$item->latitude,
+                'longitude'=>$item->longitude,
+                'date'=>$item->date,
+                'time_slot'=> $item->time_slot == 1 ? 'Now' : ($item->time_slot == 2 ? '09Am-12pm' : ($item->time_slot == 3 ? '12pm-03pm' : ($item->time_slot == 4 ? '03pm-06pm' : '-'))),
+                'total'=> $item->total,
+                'customer'=>  $item->customer?$item->customer->name:'-',
+                'driver'=>$item->driver?$item->driver->name:'-',
+                'driver_tip'=> $item->driver_tip,
+                'status'=> $item->status == 0 ? 'Ordered' : ($item->status == 1 ? 'Confirmed' : ($item->status == 2 ? 'On the way' : ($item->status == 3 ? 'order processing' : ($item->status == 4 ? 'Delivered' : ($item->status == 5 ? 'Canceled' : '-'))))),
+                'cancel_reason'=>$item->cancel_reason,
+            ];
+        }
+
+        return collect($data);
+    }
+
+    public function headings() :array
+    {
+        return [
+            'ID',
+            'Location',
+            'Latitude',
+            'Longitude',
+            'Delivery Date',
+            'Time Slot',
+            'Total',
+            'Customer',
+            'Driver',
+            'Driver Tips',
+            'Order Status',
+            'Cancel Reason',
+        ];
     }
 }
