416efd13f6eeed25fd9b70799bfd6e91551aa5ff
jaydeep-adsum <jaydeep.adsum@gmail.com>
2022-08-31 17:06:04 +0530

    order export

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -18,18 +18,19 @@ class OrderExport implements FromCollection, WithHeadings
         $data = [];
         foreach ($order as $item){
             $data[] = [
-                'order_id'=>$item->id,
-                'invoice_id'=>$item->id,
-                'location'=>$item->address->location,
-                'latitude'=>$item->address->latitude,
-                'longitude'=>$item->address->longitude,
-                'date'=>$item->date,
-                'time_slot'=> $item->time_slot == 1 ? 'Now' : ($item->time_slot == 2 ? '09Am-12pm' : ($item->time_slot == 3 ? '12pm-03pm' : ($item->time_slot == 4 ? '03pm-06pm' : '-'))),
-                'total'=> $item->total,
+                'order_id'=>$item->order_invoice,
+                'invoice_id'=>$item->invoice_id,
                 'customer'=>  $item->customer?$item->customer->name:'-',
-                'driver'=>$item->driver?$item->driver->name:'-',
-                'driver_tip'=> $item->driver_tip,
+                'location'=>$item->address->location,
+                'date'=>$item->date,
+                'time_slot'=> $item->time_slot,
+                'payment_method'=> $item->payment_method,
+                'payment_status'=> ($item->payment&&$item->payment->payment_status=="paid")?'Paid':'Unpaid',
                 'status'=> $item->status == 0 ? 'Ordered' : ($item->status == 1 ? 'Confirmed' : ($item->status == 2 ? 'On the way' : ($item->status == 3 ? 'order processing' : ($item->status == 4 ? 'Delivered' : ($item->status == 5 ? 'Canceled' : '-'))))),
+                'driver'=>$item->driver?$item->driver->name:'-',
+                'driver_number'=>$item->driver?$item->driver->mobile:'-',
+                'total'=> $item->total,
+                'driver_tip'=> $item->driver_tip,
                 'cancel_reason'=>$item->cancel_reason,
             ];
         }
@@ -40,17 +41,19 @@ class OrderExport implements FromCollection, WithHeadings
     public function headings() :array
     {
         return [
-            'ID',
+            'Order ID',
+            'Invoice ID',
+            'Customer',
             'Location',
-            'Latitude',
-            'Longitude',
             'Delivery Date',
             'Time Slot',
-            'Total',
-            'Customer',
-            'Driver',
-            'Driver Tips',
+            'Payment Method',
+            'Payment Status',
             'Order Status',
+            'Driver',
+            'Driver Number',
+            'Total',
+            'Driver Tips',
             'Cancel Reason',
         ];
     }
